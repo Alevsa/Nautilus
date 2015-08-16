f6e985dad36857a0be550fefce92ca6c17336ff4
JohnFrogman <Alex_R_HAyes@hotmail.co.uk>
2015-08-16 07:40:07 +0100

    Day night cycle, bunch of models too.

---
+++ Nautilus/Assets/Code/Environment/DayNightCycle.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DayNightCycle : MonoBehaviour 
+{
+	// Very basic for now, might get more complex later. To account for light changing colour through the cycle.
+	public float timeflow = 1f;
+	void Update () 
+	{
+		gameObject.transform.Rotate(timeflow*Vector3.down*Time.deltaTime);
+	}
+}
---
+++ Nautilus/Assets/Code/Environment/FloatingRock.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloatingRock : MonoBehaviour {
+
+	// Just a really basic script that makes rocks drift a little.
+	public float speed = 1;
+	private Vector3 drift = new Vector3(0.2F, 0, 0);
+	void start ()
+	{
+		drift.x = Random.Range (-1, 1);
+		drift.y = Random.Range(-1, 1);
+		drift.z =  Random.Range(-1, 1);
+	}
+
+	void Update () 
+	{
+		gameObject.transform.Rotate (speed * drift * Time.deltaTime);
+	}
+}
