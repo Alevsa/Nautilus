97b362387059a9b2dd324f4c966dfd41655eca92
JohnFrogman <Alex_R_HAyes@hotmail.co.uk>
2015-08-19 23:23:17 +0100

    Butchered the weather system some more

--- Nautilus/Assets/Code/Environment/Weather.cs
+++ Nautilus/Assets/Code/Environment/Weather.cs
@@ -3,37 +3,57 @@ using System.Collections;
 
 public class Weather : MonoBehaviour 
 {
+	#region Particle systems and focus (The player, though this could be changed to different biomes)
 	// This whole script is quite rought but it's functional.
 	public GameObject focus;
 	public GameObject rainParticles;
+	#endregion
+	#region Variables, pretty self explanatory from the names.
+	public float maxVolume = 0.8f;
 	// rate at which the sound effects change in volume
-	public float volumeIncreaseSpeed = 0.01f;
+	public float fadeTime = 1f;
+	private float _fadeTime = 1f;
 	// This number is a percentage per frame
 	public float rainChance = 1f;
-	public float snowChance = 1f;
-	// Duration in minutes
-	public float _duration = 1f;
-	private float totalProbability;
-	private float duration;
+	//public float snowChance = 1f;
+	// Duration in seconds
+	public float _duration = 60f;
+	private float duration = 60f;
+
 	private bool weatherActive = false;
+	// Things that need fixing
+	private float totalProbability;
+	private AudioSource rainAudio;
+	#endregion
 
 	void start()
 	{
-		totalProbability = rainChance + snowChance;
-		duration = _duration * 3600f;
+		duration = _duration;
+		fadeTime = _fadeTime;
+		rainAudio = rainParticles.GetComponent <AudioSource>();
+		//totalProbability = rainChance + snowChance;	
 	}
 
 	void Update () 
 	{
+		#region Handles the fading in/out of the audio
+		if (weatherActive) {
+			fadeRainAudio (true, rainParticles);
+		}
+		if (_duration <= fadeTime && weatherActive)
+		{
+			fadeRainAudio(false,rainParticles); 
+		}
+		#endregion
+
 		#region Keeps the weather focused on the player, it wont rotate with her though
 		gameObject.transform.position = focus.transform.position;
 		#endregion
 
-			#region If it's not raining, chance to trigger rain [I'll change this to allow a chance of snow and other stuff later.]
+		#region If it's not raining, chance to trigger rain [I'll change this to allow a chance of snow and other stuff later.]
 		if (!weatherActive) {
 			if (Random.value < rainChance / 100f) {
 				rainParticlesToggle ();
-				fadeAudio(rainParticles, true);
 			}
 		} 
 		#endregion
@@ -42,28 +62,32 @@ public class Weather : MonoBehaviour
 		else {
 			if (duration == 0) {
 				rainParticlesToggle ();
-				duration = _duration * 3600f;
+				duration = _duration;
 			} else {
-				duration--;
+				duration -= Time.deltaTime;
 			}
 		}
 		#endregion
-	}
 
+	}
+	#region Toggles the rain particles on and off
 	void rainParticlesToggle()
 	{
 		weatherActive = !weatherActive;
 		rainParticles.SetActive (weatherActive);
 	}
+	#endregion
 
-	void fadeAudio(GameObject weatherEffect, bool fadingIn)
+	#region Fades the audio in [and later will fade it out]
+	void fadeRainAudio(bool fadingIn, GameObject source)
 	{
-		AudioSource audio = weatherEffect.GetComponent <AudioSource>();
-		if (audio.volume < 0.8f && fadingIn) {
-			audio.volume += volumeIncreaseSpeed;
-		} else if (!fadingIn) 
+		AudioSource audio = source.GetComponent<AudioSource>();
+		if (audio.volume < maxVolume && fadingIn) {
+			audio.volume += (maxVolume/fadeTime) * Time.deltaTime;
+		} else if (audio.volume >= 0f && !fadingIn) 
 		{
-			audio.volume -= volumeIncreaseSpeed;
+			audio.volume -= (maxVolume/fadeTime) * Time.deltaTime;
 		}
 	}
+	#endregion
 }
