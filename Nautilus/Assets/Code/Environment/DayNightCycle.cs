841480a4d842fd50a5548b73d0a5d1d3986c8e00
JohnFrogman <Alex_R_HAyes@hotmail.co.uk>
2015-08-21 13:18:44 +0100

    Fixed day night cycle
    If I don't explain what the problem was you probably wont notice the fix

--- Nautilus/Assets/Code/Environment/DayNightCycle.cs
+++ Nautilus/Assets/Code/Environment/DayNightCycle.cs
@@ -3,16 +3,42 @@ using System.Collections;
 
 public class DayNightCycle : MonoBehaviour 
 {
-	// Very basic for now, might get more complex later.
-	// in minutes
+	// In minutes
+	#region Public variables for for human consumption
 	public float _lengthOfDay = 2f;
+	public float _maxMoonIntensity = 0.3f;
+	public float _maxSunIntensity = 1f;
+	public GameObject _sun;
+	public GameObject _moon;
+	#endregion
+	#region converted for use in the machine
+	private float maxSunIntensity = 0.5f;
+	private float maxMoonIntensity = 0.15f;
 	private float lengthOfDay = 120f;
+	private Light sun;
+	private Light moon;
+	#endregion
+
 	void Start()
 	{
+		maxMoonIntensity = _maxMoonIntensity/2f;
+		maxSunIntensity = _maxSunIntensity/2f;
+
 		lengthOfDay = _lengthOfDay * 60;
+		sun = _sun.GetComponent<Light>();
+		moon = _moon.GetComponent<Light>();
 	}
+
 	void Update () 
 	{
 		gameObject.transform.Rotate((360f/lengthOfDay)*Vector3.left*Time.deltaTime);
+		sun.intensity = celestialBodyIntensity(gameObject.transform.rotation.x, 1, maxSunIntensity);
+		moon.intensity = celestialBodyIntensity(gameObject.transform.rotation.x, -1, maxMoonIntensity);
+
+	}
+	// The intensity will go to zero when the opposite celestial body is up to stop strange illumination coming from the earth during hte night time.
+	float celestialBodyIntensity(float time, float phase, float maxIntensity)
+	{
+		return maxIntensity*(phase*Mathf.Sin(0.5f*time)+1);
 	}
 }
