b205ff1f2170b92659dd7221ac6cb623cc3e7802
JohnFrogman <Alex_R_HAyes@hotmail.co.uk>
2015-08-24 08:14:46 +0100

    Half begun is not well done
    Nothing of interest, it's better you don't look at it.

---
+++ Nautilus/Assets/Code/Enemies/Ram.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ram : MonoBehaviour {
+
+	public GameObject focus;
+	public float maxSpeed;
+	void Start () 
+	{
+		shouldICharge();
+	}
+
+	void charge()
+	{
+		// Charge toward focus for a while
+		shouldICharge();
+	}
+
+	void getIntoPosition()
+	{
+		// Get into position
+		charge();
+	}
+
+	void shouldICharge()
+	{
+		// if aligned toward player and far away enough
+		if (true)
+		{
+			charge();
+		}
+		else 
+		{
+			getIntoPosition();
+		}
+	}
+
+}
