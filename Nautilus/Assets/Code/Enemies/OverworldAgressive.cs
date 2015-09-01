fc7440f810c13ea34824e38d70f94947491cf84b
JohnFrogman <Alex_R_HAyes@hotmail.co.uk>
2015-09-01 09:01:26 +0100

    A bunch of empty scripts
    Didn't want to lose them though <3

---
+++ Nautilus/Assets/Code/Enemies/OverworldAgressive.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OverworldAgressive : MonoBehaviour 
+{
+	private bool spottedFocus = false;
+	private GameObject focus;
+
+	void Start () 
+	{
+		focus = GameObject.FindGameObjectWithTag("player");
+	}
+	void Update () 
+	{
+		spottedFocus = canISeeMyFocus();
+		moveToFocus();
+	}
+	void moveToFocus()
+	{
+		gameObject.transform.rotation.SetLookRotation(focus)
+	}
+
+	bool canISeeMyFocus()
+	{
+		return true;
+	}
+}
