a983da6879cbf55bcd9f7fbc5728b0428a8d6504
JohnFrogman <Alex_R_HAyes@hotmail.co.uk>
2015-09-10 22:29:38 +0100

    Started work on the revamped weather script

--- Nautilus/Assets/Code/Environment/Weather.cs
+++ Nautilus/Assets/Code/Environment/Weather.cs
@@ -1,109 +1,55 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Weather : MonoBehaviour 
 {
-	#region Particle systems and focus (The player, though this could be changed to different biomes)
-	// This whole script is quite rought but it's functional.
-	public GameObject focus;
-	public GameObject rainParticles;
-	#endregion
-
-	#region Variables, pretty self explanatory from the names.
-	// How heavily the rain falls. (Max emissivity of the emitter.)
-	public float _maxWeatherDensity = 5000;
-	public float maxVolume = 0.8f;
-	// rate at which the sound effects change in volume
-	public float _fadeTime = 1f;
-	// This number is a percentage per frame
-	public float rainChance = 1f;
-	//public float snowChance = 1f;
-
-	// Duration in seconds
-	public float _duration = 60f;
-	private float duration = 60f;
-
-	private bool weatherActive = false;
-	// This will be used to choose what weather happens.
-	private float totalProbability;
-	// Audio Sources
-	private AudioSource rainAudio;
-	// Particle emissivity
-	private EllipsoidParticleEmitter rainEmitter;
-	#endregion
-
-	#region Start method, just gets the audio component(s) and sets up some stuff.
+	public GameObject focus; // The player most likely
+	private List<WeatherEffect> weatherEffects;
+	public float chanceForWeather = 0.001f;
+	public float fadeRate = 1f;
+	private bool active = false;
+	
 	void Start()
 	{
-		duration = _duration;
-		rainAudio = rainParticles.GetComponent <AudioSource>();
-		rainEmitter = rainParticles.GetComponent<EllipsoidParticleEmitter>();	
-		//totalProbability = rainChance + snowChance;	
-	}
-	#endregion
-
-	#region Update method
-	void Update () 
-	{
-		#region Handles the fading in/out of the effect
-		if (weatherActive) 
+		for (int i = 0; i < gameObject.transform.GetChildCount(); i++)
 		{
-			if (duration >= _fadeTime)
-			{
-				fadeEffect (true, rainAudio, rainEmitter);
-			}
-			else if (duration <= _fadeTime)
-			{
-				fadeEffect(false,rainAudio, rainEmitter); 
-			}
+			GameObject x = gameObject.transform.GetChild(i);
+			weatherEffects.Add(x.GetComponent<WeatherEffect>());
 		}
-		#endregion
-
-		#region Keeps the weather focused on the player, it wont rotate with her though
-		gameObject.transform.position = focus.transform.position;
-		#endregion
-
-		#region If it's not raining, chance to trigger rain [I'll change this to allow a chance of snow and other stuff later.]
-		if (!weatherActive) {
-			if (Random.value < rainChance / 100f) {
-				rainParticlesToggle ();
-			}
-		} 
-		#endregion
-
-		#region Else if the duration is up, stop raining or tick down the duration
-		else {
-			if (duration <= 0) {
-				rainParticlesToggle ();
-				duration = _duration;
-			} else {
-				duration -= Time.deltaTime;
-			}
-		}
-		#endregion
-
 	}
-	#endregion
-
-	#region Toggles the rain particles on and off
-	void rainParticlesToggle()
+	
+	void Update()
 	{
-		weatherActive = !weatherActive;
-		rainParticles.SetActive (weatherActive);
-	}
-	#endregion
-
-	#region Fades the effect in/out
-	void fadeEffect(bool fadingIn, AudioSource audio, EllipsoidParticleEmitter particles)
-	{
-		if (audio.volume < maxVolume && fadingIn) {
-			audio.volume += (maxVolume/_fadeTime) * Time.deltaTime;
-			particles.maxEmission += (_maxWeatherDensity/_fadeTime) * Time.deltaTime;
-		} else if (audio.volume >= 0f && !fadingIn) 
+		if (Random.value < chanceForWeather && !active)
 		{
-			audio.volume -= (maxVolume/_fadeTime) * Time.deltaTime;
-			particles.maxEmission -= (_maxWeatherDensity/_fadeTime) * Time.deltaTime;
+			WeatherEffect active = weatherRoulette();
+			
+			active.effect.SetActive = true;
 		}
 	}
-	#endregion
+	
+	WeatherEffect weatherRoulette()
+	{
+		float totalProbability = 0;
+		foreach (WeatherEffect effect in weatherEffects)
+		{
+			totalProbability += effect.probability;
+		}
+		float spin = Random.value*totalProbability;
+		float total = 0;
+		foreach (WeatherEffect effect in weatherEffects)
+		{
+			total += effect.probability;
+			if (spin < total)
+			{
+				return effect;
+			} 
+		}
+	}
+	
+	IEnumerator fadeEffect(WeatherEffect effect, bool fadingIn)
+	{
+		while (effect.effect.get < effect.maxParticles )
+	}	
 }
---
+++ Nautilus/Assets/Code/Environment/WeatherEffect.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeatherEffect : MonoBehaviour 
+{
+	public float probability;
+	public GameObject effect;
+	public float maxParticles;
+	public float maxVolume;
+	public float duration;
+}
