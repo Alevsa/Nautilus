c7916fdcb45498bbed0aed3775e74954456955e7
JohnFrogman <Alex_R_HAyes@hotmail.co.uk>
2015-09-28 08:49:02 +0100

    Placeholder commit
    While I go shower and apply for jobs, real one later.

---
+++ Nautilus/Assets/Code/Player/Battle/PlayerMovementSpaceBattle.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovementSpaceBattle : MonoBehaviour 
+{
+	private PlayerStatsSpaceBattle stats;
+	private Rigidbody body;
+	private GameObject playerCam;
+	private float speed;
+	
+	
+	void Start()
+	{
+		playerCam = GameObject.FindGameObjectsWithTag("MainCamera");
+		stats = GetComponent<PlayerStatsSpaceBattle> ();
+		body = GetComponent<Rigidbody> ();
+	}
+	
+	void FixedUpdate()
+	{
+		movement();
+	}
+	
+	void movement()
+	{
+		body.AddRelativeForce(Vector3.forward * speed);
+		transform.rotation = Quaternion.RotateTowards(transform.rotation, GetComponent<Camera>().transform.rotation, stats.turnRate);
+	}
+	
+	void acceletate()
+	{
+		if (speed < stats.maxSpeed)
+		{
+			speed += stats.acceleration;
+		}
+	}
+	
+	
+	
+}
---
+++ Nautilus/Assets/Code/Player/Battle/PlayerStatsSpaceBattle.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStatsSpaceBattle : MonoBehaviour 
+{
+	public float health;
+	public float turnRate;
+	public float maxSpeed;
+	public float acceleration;
+}
